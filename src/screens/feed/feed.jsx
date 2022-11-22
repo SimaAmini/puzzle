cb055f4e3b77d466553bf0f31a42fba68902f65d
Sima Amini <sima.amini@connect2wow.com>
2022-11-22 17:16:04 +0330

    redirect to post detail

--- src/screens/feed/feed.jsx
+++ src/screens/feed/feed.jsx
@@ -1,14 +1,22 @@
+import { useNavigation } from '@react-navigation/native';
 import { FlatList, StyleSheet, TouchableHighlight } from 'react-native';
 import { Post, ItemSeparator } from '../../components';
 import Screen from '../../layout/screen';
-
+import { screens } from '../../constants';
 export const Feed = () => {
+  const navigation = useNavigation();
+
   return (
     <Screen style={styles.screen}>
       <FlatList
         data={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }]}
         renderItem={({ item }) => (
-          <TouchableHighlight key={item.id} onPress={() => console.log(item)}>
+          <TouchableHighlight
+            key={item.id}
+            onPress={() => navigation.navigate(screens.POST_DETAIL)}
+            activeOpacity={1}
+            underlayColor="transparent"
+          >
             <Post />
           </TouchableHighlight>
         )}
--- src/screens/profile/profile.jsx
+++ src/screens/profile/profile.jsx
@@ -1,11 +1,21 @@
+import { useNavigation } from '@react-navigation/native';
 import { useState } from 'react';
-import { FlatList, Image, StyleSheet, View } from 'react-native';
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  TouchableHighlight,
+  View,
+} from 'react-native';
 
 import { ProfileHeader } from '../../components/profile-header';
+import { screens } from '../../constants';
 import Screen from '../../layout/screen';
 
 export const Profile = () => {
   const [dataSource, setDataSource] = useState([{}, {}, {}, {}, {}, {}]);
+
+  const navigation = useNavigation();
 
   return (
     <Screen>
@@ -21,12 +31,19 @@ export const Profile = () => {
                 margin: 1,
               }}
             >
-              <Image
-                style={styles.imageThumbnail}
-                source={{
-                  uri: 'https://api.lorem.space/image/watch?w=150&h=150',
-                }}
-              />
+              <TouchableHighlight
+                key={item.id}
+                onPress={() => navigation.navigate(screens.POST_DETAIL)}
+                activeOpacity={1}
+                underlayColor="transparent"
+              >
+                <Image
+                  style={styles.imageThumbnail}
+                  source={{
+                    uri: 'https://api.lorem.space/image/watch?w=150&h=150',
+                  }}
+                />
+              </TouchableHighlight>
             </View>
           )}
           numColumns={3}
